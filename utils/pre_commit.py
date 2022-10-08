59016eecb16dacc73018febe193b4b8e69d6e2e1
Rafael Fernandes <rflgf0@gmail.com>
2022-10-08 14:30:35 -0300

    fix: clang-format on wrong file extensions n' dirs

--- utils/pre_commit.py
+++ utils/pre_commit.py
@@ -1,18 +1,30 @@
 import os
+import logging
 
 def clang_format(project_dirs : list):
 	dirs_to_format = []
+	ignored_dirs = ["vendor"]
 	
 	for proj_dir in project_dirs:
-		for dir, _, files in os.walk(proj_dir):
-			for filename in files:
-				if filename.endswith(".cpp") or filename.endswith(".h"):
-					dirs_to_format.append(os.path.join(dir, "*"))
+		for dir, _, _ in os.walk(proj_dir):
+			should_format = True
+			for ign_dir in ignored_dirs:
+				if ign_dir in dir:
+					should_format = False
 					break
+			if not should_format:
+				continue
+			dirs_to_format.append(os.path.join(dir, "*.cpp"))
+			dirs_to_format.append(os.path.join(dir, "*.h"))
 	
+	# FIXME(rafael): this warns on directories with no *.h
+	# or *.cpp files, maybe look into how to suppress that.
+	logging.getLogger("sol").info('applying clang-format, "invalid'
+		' argument" warnings can be ignored.')
 	os.system("clang-format -i " + ' '.join(dirs_to_format))
 
 if __name__ == "__main__":
-	projects = ["sol", "eclipse", "example_app"]
+	logging.basicConfig(level=logging.INFO)
 
+	projects = ["sol", "eclipse", "example_app"]
 	clang_format(projects)
